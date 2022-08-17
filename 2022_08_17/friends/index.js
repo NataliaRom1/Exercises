c2f627e956e4adf4996096ca494f8fb877d214fe
Nata <Natalia.Romanovskaia@skoltech.ru>
2022-08-17 19:44:51 +0300

    solved tasks for 17.08.2022

---
+++ 2022_08_17/friends/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Генерация HTML списка друзей
+ * @param {Friend[]} friends
+ * @return {HTMLUListElement}
+ */
+
+
+
+
+function makeFriendsList(friends) {
+    let ul = document.createElement("ul");
+    for (let elem of friends) {
+        let li = document.createElement("li");
+        li.innerHTML = `${elem.firstName} ${elem.lastName}`;
+        ul.appendChild(li);
+    }
+    return ul;
+}
+
+
